c0059de96df4331bd7a21843f5aa4b8ab59e1e59
Hanna Jubran <hannaj@corp.deltagalil.com>
2020-03-03 21:02:04 +0200

    Updates

---
+++ precache-manifest.d9278cc78d1e4971fa461f930f57d1af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a063829b9c64ab5a3f15950d78f41b6",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "58b0a91a8cde9aa43f88",
+    "url": "/reacttest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7ba0bab249c2eba2a72c",
+    "url": "/reacttest/static/js/2.e3031ffd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reacttest/static/js/2.e3031ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b0a91a8cde9aa43f88",
+    "url": "/reacttest/static/js/main.cdd7211f.chunk.js"
+  },
+  {
+    "revision": "a9162847be1cabe94758",
+    "url": "/reacttest/static/js/runtime-main.666212cc.js"
+  }
+]);
